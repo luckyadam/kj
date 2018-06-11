1fc2cd4aeef091f8b69b79d1f28f867337a91471
luckyadam <weitaozsh@gmail.com>
2018-06-11 15:15:24 +0800

    redux 示例

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,25 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
-import A from '../../components/A'
+import { View, Button } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+import A from '../../components/A/A'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
 import './index.scss'
 
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
 export default class Index extends Component {
   config = {
     navigationBarTitleText: '首页'
@@ -18,15 +35,21 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
-  handleX = () => {
-    console.log('sdsd')
+  goto = () => {
+    Taro.navigateTo({
+      url: '/pages/index2/index'
+    })
   }
 
   render () {
     return (
       <View className='index'>
-        <Text onClick={this.handleX}>122</Text>
+        <Button className='add_btn' onClick={this.props.add}>+</Button>
+        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+        <View>{this.props.counter.num}</View>
         <A />
+        <Button onClick={this.goto}>走你</Button>
       </View>
     )
   }
